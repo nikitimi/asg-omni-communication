1b7e11534d57df6fc319c5e535e20a0d92fa3f07
Gian Carlo Carranza <giancarloacarranza@gmail.com>
2025-01-30 03:01:55 +0800

    feat!: added mms history.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "clicksend-api-downloads.s3.ap-southeast-2.amazonaws.com",
+        pathname: "/_private/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,29 +3,54 @@ import React from "react";
 import createAuthorization from "@/utils/createAuthorization";
 import SMSHistory from "@/components/SMSHistory";
 import Navigation from "@/components/Navigation";
+import SendSMS from "@/components/SendSMS";
+import type { SMSHistory as SH } from "@/utils/schemas/smsHistory";
 
 export default async function Dashboard() {
-  const historyEndpoint = new URL(
+  const smsHistoryEndpoint = new URL(
     "./sms/history",
     process.env.NEXT_PUBLIC_CLICKSEND_URL
   );
-  const response = await fetch(historyEndpoint, {
-    headers: {
-      method: "GET",
-      authorization: createAuthorization(),
-    },
-  });
+  const mmsHistoryEndpoint = new URL(
+    "./mms/history",
+    process.env.NEXT_PUBLIC_CLICKSEND_URL
+  );
+  const requests = [
+    fetch(smsHistoryEndpoint, {
+      headers: {
+        method: "GET",
+        authorization: createAuthorization(),
+      },
+    }),
+    fetch(mmsHistoryEndpoint, {
+      headers: {
+        method: "GET",
+        authorization: createAuthorization(),
+      },
+    }),
+  ];
+  const responses = await Promise.allSettled(requests);
+  const responseValues: SH[] = [];
 
-  if (!response.ok) {
-    console.log(await response.json());
-    return <div>Error in getting history</div>;
+  for (const response of responses.filter(
+    (response) => response.status === "fulfilled"
+  )) {
+    const json = await response.value.json();
+    if (response.value.ok) {
+      responseValues.push(json);
+    }
+  }
+
+  if (responseValues.length !== requests.length) {
+    return <div>Requests not all fullfilled.</div>;
   }
 
   return (
     <div>
       <Navigation />
       <p>Dashboard</p>
-      <SMSHistory data={response.json()} />
+      <SendSMS />
+      <SMSHistory responses={responseValues} />
     </div>
   );
 }
